387bb29b092e8672828506669812615683be5821
Hooman <patriot_sh@yahoo.com>
2022-12-06 22:32:50 +0330

    bootstrap installation and using amazon color

--- amazona/backend/server.js
+++ amazona/backend/server.js
@@ -1,13 +1,13 @@
-import express from "express";
+import express from 'express';
 import data from './data.js';
 
 const app = express();
-
-app.get('/api/products', (req,res) => {
-    res.send(data.products);
-} );
+// test
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`server at http://localhost:${port}`)
+  console.log(`serve at http://localhost:${port}`);
 });
--- amazona/frontend/src/screens/ProductScreen.js
+++ amazona/frontend/src/screens/ProductScreen.js
@@ -3,7 +3,10 @@ import { useParams } from "react-router-dom";
 function ProductScreen() {
   const params = useParams();
   const { slug } = params;
-  return <div>{slug}</div>;
+  return (
+    <div>
+      <h1>{slug}</h1>
+    </div>
+  );
 }
-
 export default ProductScreen;
