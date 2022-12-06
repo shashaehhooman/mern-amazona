54969971cdddb80d2d1c3b83a0dbef6381b79dd3
Hooman <patriot_sh@yahoo.com>
2022-12-06 17:55:42 +0330

    video4-routing-react-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 function App() {
   return (
-    <div>
-      <header>
-    <a href="/">amazona</a>
-      </header>
-      <main>
-      List Products
-    </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <a href="/">amazona</a>
+        </header>
+        <main>
+          <Routes>
+          <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+          
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom";
+
+function ProductScreen() {
+    const params=useParams();
+    const {slug}=params;
+    return (  
+        <div>{slug}</div>
+    );
+}
+
+export default ProductScreen;
